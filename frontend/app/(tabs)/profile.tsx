1fcc2d063983072fb9c2c4de91428e820fb72c79
Nathan Grilliot <grillinr@mail.uc.edu>
2024-12-15 19:05:04 -0500

    Started adding tab for profile page

---
+++ frontend/app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { Post } from "@/components/Post";
+import ScrollView from "@/components/ScrollView";
+import CreatePost from "@/components/CreatePost";
+import TopBar from "@/components/ui/TopBar";
+
+export default function ProfileScreen() {
+  return (
+    <>
+      {/* <TopBar /> */}
+      <ScrollView></ScrollView>
+    </>
+  );
+}
