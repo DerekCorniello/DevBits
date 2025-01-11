642b5303e444d21cd7828b2bd2cf123d8020189c
Nathan Grilliot <grillinr@mail.uc.edu>
2025-01-11 17:57:56 -0500

    Added follower/following count to profile page

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -2,12 +2,19 @@ import { ThemedText } from "@/components/ThemedText";
 import TopBar from "@/components/ui/TopBar";
 import User from "@/components/User";
 import { UserProps, ProjectProps, CommentProps } from "@/constants/Types";
+import { View, Text, StyleSheet, Animated } from "react-native";
 import React from "react";
 import ScrollView from "@/components/ScrollView";
 const username = "dev_user1";
 
 export default function ProfileScreen() {
   const [currentUser, setCurrentUser] = React.useState<UserProps | null>(null);
+  const [userFollowers, setCurrentUserFollowers] = React.useState<
+    number | null
+  >(null);
+  const [userFollows, setCurrentUserFollows] = React.useState<number | null>(
+    null
+  );
   // const [currentProjects, setCurrentProjects] =
   // React.useState<ProjectProps | null>(null);
 
@@ -19,6 +26,30 @@ export default function ProfileScreen() {
       setCurrentUser(userData);
     }
     fetchUser();
+  }, []);
+
+  React.useEffect(() => {
+    async function fetchFollowers() {
+      const response = await fetch(
+        `http://localhost:8080/users/${username}/followers`
+      );
+      const userFollowers = await response.json();
+      console.log(userFollowers);
+      setCurrentUserFollowers(userFollowers);
+    }
+    fetchFollowers();
+  }, []);
+
+  React.useEffect(() => {
+    async function fetchFollows() {
+      const response = await fetch(
+        `http://localhost:8080/users/${username}/follows`
+      );
+      const userFollows = await response.json();
+      console.log(userFollows);
+      setCurrentUserFollows(userFollows);
+    }
+    fetchFollows();
   }, []);
 
   // React.useEffect(() => {
@@ -45,6 +76,13 @@ export default function ProfileScreen() {
             created_on={currentUser.created_on}
             picture={currentUser.picture}
           />
+          <View>
+            <ThemedText type="default">
+              <b>{userFollowers ?? 0}</b> followers • <b>{userFollows ?? 0}</b>{" "}
+              following
+              {/* <b>{projects}</b> streams */}
+            </ThemedText>
+          </View>
           <ThemedText type="default">Projects</ThemedText>
         </ScrollView>
       )}
